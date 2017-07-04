0e8b9a9497bc527d98610514ec70d08036d3ec7f
jopaulo1984 <jopaulo1984@gmail.com>
2017-07-03 22:52:32 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -d "/usr/share/notas-adesivas" ];
+then
+    rm -R /usr/share/notas-adesivas
+    rm /usr/bin/notas-adesivas
+fi
+
+
+$dirnotas="/usr/share/notas-adesivas"
+
+echo "instalando..."
+mkdir /usr/share/notas-adesivas
+mkdir /usr/share/notas-adesivas/notaslib
+cp -R ./codigos/notaslib/ /usr/share/notas-adesivas/
+cp ./codigos/notas-adesivas-gui.py /usr/share/notas-adesivas/notas-adesivas-gui.py
+cp ./codigos/notaslib/icones/notas-adesivas.ico /usr/share/notas-adesivas/notas-adesivas.ico
+#cp ./notas-adesivas.desktop /usr/share/applications/notas-adesivas-gui.desktop
+ln -s /usr/share/notas-adesivas/notas-adesivas-gui.py /usr/bin/notas-adesivas
+chmod 777 /usr/bin/notas-adesivas
+chmod 777 /usr/share/notas-adesivas/notas-adesivas.ico
+echo "Instalação concluída."
+
+
