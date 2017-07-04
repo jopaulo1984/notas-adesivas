7cec02a89baac6e2874bef728f633869ab2bd8df
jopaulo1984 <jopaulo1984@gmail.com>
2017-07-04 20:10:51 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,6 @@ cp ./codigos/notaslib/icones/notas-adesivas.ico /usr/share/notas-adesivas/notas-
 ln -s /usr/share/notas-adesivas/notas-adesivas-gui.py /usr/bin/notas-adesivas
 chmod 777 /usr/bin/notas-adesivas
 chmod 777 /usr/share/notas-adesivas/notas-adesivas.ico
-/etc/init.d/update-rc.d notas-adesivas defaults
 echo "Instalação concluída."
 
 
