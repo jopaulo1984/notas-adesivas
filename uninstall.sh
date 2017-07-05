31a0ebeed79f380c430798d1fd4679172eff1dda
jopaulo1984 <jopaulo1984@gmail.com>
2017-07-04 21:34:43 -0300

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+remove() {
+    if [ $1 = "-f" ];
+    then
+        rm $2
+    else
+        if [ -d $2 ];
+        then
+            rm -R $2
+        fi
+    fi
+}
+
+remove "-d" "/usr/share/notas-adesivas"
+remove "-f" "/usr/bin/notas-adesivas"
+remove "-f" "~/.config/autostart/notas-adesivas-gui.desktop"
+remove "-f" "/usr/share/applications/notas-adesivas-gui.desktop"
